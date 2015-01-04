07bacdc7e2ac078f047f54d06090e9be65a853bd
iceweasel3306 <iceweasel3306@gmail.com>
2015-01-04 10:10:03 -0800

    modified:   README.md 	modified:   config 	modified:   restarter 	modified:   server_cnfg 	modified:   start.sh 	modified:   startupwsc

--- README.md
+++ README.md
@@ -19,17 +19,17 @@
     Description:
    A collection of tools mostly written in bash designed to help manage a TrinityCore server on a Linux distrubtion operating system.  Bash commands are not needed to operate the Server Manager making use of the tools quick and easy.
     The tools included in the project consist of:
-[*]A Main menu (trinity.sh), this menu allows easy navigation and use of the other tools.
-[*]Two different startup styles, one for service checking and one for a quick startup to start your TrinityCore Server. Both styles start the server in screen daemons for easy management.
-[*]A separate service check that can be performed outside of the startup scripts.
-[*]An account manager that includes account creation, password updating, and account deletion
-[*]A privledged account permission manager (In Progress)
-[*]A server configuration manager that includes easy auth, world, apache, and mysql configuring. (In Progress)
-[*]A backup manager capable of writing full backups of sql servers, apache servers, binaries, and configuration files.  The backup manager can also launch a background process to auto backup at certain times and store/upload selected files.  Sql server backup style is configurable to either a *.sql file or on a seperate db.  All backup files are configured to compress and store locally or via ftp.(TODO)
-[*]A server restarter with crash data gathering tools that runs in the background
-[*]Built into the main menu are ways to easily attach and detach in the server screen daemons.
-[*]A quick access to MySQL command line
-[*]Most tools/modules/scripts include logging procedures for debugging, referencing, and security purposes.
-[*]A configurable file to adjust to your server and machine.
+*A Main menu (start.sh), this menu allows easy navigation and use of the other tools.
+*Two different startup styles, one for service checking and one for a quick startup to start your TrinityCore Server. Both styles start the server in screen daemons for easy management.
+*A separate service check that can be performed outside of the startup scripts.
+*An account manager that includes account creation, password updating, and account deletion
+*A privledged account permission manager (In Progress)
+*A server configuration manager that includes easy auth, world, apache, and mysql configuring. (In Progress)
+*A backup manager capable of writing full backups of sql servers, apache servers, binaries, and configuration files.  The backup manager can also launch a background process to auto backup at certain times and store/upload selected files.  Sql server backup style is configurable to either a *.sql file or on a seperate db.  All backup files are configured to compress and store locally or via ftp.(TODO)
+*A server restarter with crash data gathering tools that runs in the background
+*Built into the main menu are ways to easily attach and detach in the server screen daemons.
+*A quick access to MySQL command line
+*Most tools/modules/scripts include logging procedures for debugging, referencing, and security purposes.
+*A configurable file to adjust to your server and machine.
 
     I am looking to continually update this project weekly until it is something I'm proud to have made. This is in its very early stages, so please forgive me if it does not function as you wish it to function. I will work on revising, refining, and organizing the scripts better. Please feel free to contact me with ideas, requests, and friendly pointers. Thanks! 
--- start.sh
+++ start.sh
@@ -4,6 +4,49 @@
 
 source config
 
+if [ $FIRST_START == 1 ]
+    then
+	mawk '{gsub("FIRST_START=1", "FIRST_START=0"); print }' config > config1
+	mv config $CONF/config2
+	mv config1 $CONF/config
+	echo "First start: $FIRST_START"
+	echo "Use configuration manager to configure the tools to your system."
+	echo "The Server Manager is configured with its location expected within the same directory of your server files, but isn't too hard to reconfigure (see file config)"
+	echo "I'll have the configuration manager able to do that soon, right now it can configure the Server Manager for your system, binary"
+	echo "default for server files directory name is /server/"
+	echo "default for binaries is /server/bin/"
+	echo "default path for server files is /home/$USER/server/"
+	sleep 1
+	echo
+	echo
+	echo "TrinityCore Server Manager 0.7 BETA"
+	echo "Developer(s): iceweasel3306"
+	echo
+	echo "Press any key to continue..."
+	sed -n q </dev/tty
+	clear
+	echo "   This program is free software; you can redistribute it and/or modify"
+	echo "   it under the terms of the GNU General Public License as published by"
+	echo "   the Free Software Foundation; either version 2 of the License, or"
+	echo "   (at your option) any later version."
+	echo
+	echo "   This program is distributed in the hope that it will be useful,"
+	echo "   but WITHOUT ANY WARRANTY; without even the implied warranty of"
+	echo "   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+	echo "   GNU General Public License for more details."
+	echo
+	echo "   You should have received a copy of the GNU General Public License along"
+	echo "   with this program; if not, write to the Free Software Foundation, Inc.,"
+	echo "   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."
+	sleep 3
+	clear
+    else
+	echo "Server Manager 0.7 BETA"
+	echo "Developer(s): iceweasel3306"
+	echo "Copyright (c)2015 Iceweasel3306"
+	echo "License: Public GNU 2.0"
+	sleep 2
+fi
 show_menu(){
     NORMAL=`echo "\033[m"`
     MENU=`echo "\033[36m"` #Blue
@@ -17,8 +60,8 @@ show_menu(){
     echo -e "${MENU}**${NUMBER} 3)${MENU}  Service Checks ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 4)${MENU}  Account Manager ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 5)${MENU}  Account Permission Manager ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 6)${MENU}  Server Configuration(In Progress) ${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 7)${MENU}  Server Restarter(Background) ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 6)${MENU}  Configuration Manager(In progress) ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 7)${MENU}  Server Restarter - start/kill service ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 8)${MENU}  Attach to Auth Screen ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 9)${MENU}  Attach to World Screen ${NORMAL}"
     echo -e "${MENU}**${NUMBER} 10)${MENU} MySQL Command Line ${NORMAL}"
@@ -38,8 +81,9 @@ clear
 show_menu
 while [ opt != '' ]
     do
-    if [[ $opt = "" ]]; then 
-            exit;
+    if [[ $opt = "" ]]; 
+    then 
+    	exit;
     else
         case $opt in
         1) clear;
@@ -54,52 +98,80 @@ while [ opt != '' ]
 		source config
             	bash startup
 		show_menu;
-            ;;
+        ;;
 
         3) clear;
            option_picked "TrinityCore Service Checks";
 		source config
         	bash svcs
 		show_menu;
-            ;;
+        ;;
 
         4) clear;
            option_picked "TrinityCore Account Management";
 		source config
         	bash account
         	show_menu;
-           ;;
+        ;;
+	
 	5) clear;
            option_picked "TrinityCore Account Permission Manager";
         	source config
 		bash permissions
         	show_menu;
-           ;;
+        ;;
+
 	6) clear;
 	   option_picked "Server Configuration";
 	   	echo "In Progress..."
 	   	bash server_cnfg
 	   	show_menu;
-	   ;;
+	;;
+	
 	7) clear;
-	   option_picked "Server Restarter (Background)";
-	   	screen -dmS restarter $RPATH/restarter
+	   option_picked "Server Restarter Service";
+		echo -n "Start(1) or kill(0): "
+		read ans
+		    if [ $ans == 1 ]
+		    then
+			clear
+			if [ ! -p $RESTARTER_SCREEN ]
+			then		
+	   	   	    screen -dmS restarter $RPATH/restarter
+			    echo "Restarter is now running..."
+			    sleep 1
+			else
+			    echo "Restarter is already running on $RESTARTER_SCREEN"
+			    echo "Run kill option first if restarter is not running."
+		  	fi
+		    else
+			killall -9 $WORLD
+			screen -S world -X kill &>/dev/null
+			screen -wipe world
+			echo "Restarter service killed..."
+		   	sleep 1
+		    fi
 	   	show_menu;
-	   ;;
+	;;
+
 	8) clear;
 	   option_picked "Attach to Auth Server";
-	   	echo "Press Ctrl+a+d to detach from the server without shutting it down."
-	   	sleep 5
+		echo "Press Ctrl+a+d to detach from the server without shutting it down."
+		echo "Attaching to Auth screen..."
+	   	sleep 3
 	   	screen -r auth
 	   	show_menu;
-	   ;;
+	;;
+
 	9) clear;
 	   option_picked "Attach to World Server";
 	   	echo "Press Ctrl+a+d to detach from the server without shutting it down."
-	   	sleep 5
+		echo "Attaching to World screen..."
+	   	sleep 3
 	   	screen -r world
 	   	show_menu;
-	   ;;
+	;;
+
 	10) clear;
 	    option_picked "MySQL Command Line";
 	    	echo "Press Ctrl+c to exit the command line."
@@ -112,17 +184,19 @@ while [ opt != '' ]
 		clear
 	    	mysql -u $username -p$password
 	    	show_menu;
-	    ;;
-        x)exit;
+	;;
+
+        x) exit;
         ;;
 
-        \n)exit;
+        \n) exit;
         ;;
 
         *)clear;
         option_picked "Pick an option from the menu";
         show_menu;
         ;;
+
     esac
-fi
+    fi
 done
